d5370d04b8f858f8d64fac667322f9974d894780
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-02-23 20:15:39 +0530

    Fix missing /  issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:" React-Project-II-TODO_App",
+  base:"/React-Project-II-TODO_App",
   plugins: [react()],
 })
